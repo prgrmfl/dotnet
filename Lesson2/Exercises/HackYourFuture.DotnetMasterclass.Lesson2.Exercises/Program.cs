809c29ad432e7da2dbd797092ff138dc451f5e5c
prgrmcode <r0830995@student.thomasmore.be>
2022-09-25 09:17:00 +0200

    1. A simple tracker

--- Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
+++ Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+var time = new JupiterTime();
+time.Hours = 8;
+time.Minutes = 40;
+
+PrintTime(time);
+
+
+static void PrintTime(JupiterTime j)
+{
+    Console.WriteLine($"{j.Hours}:{j.Minutes}");
+}
+
+class JupiterTime
+{
+    public int Hours
+    {
+        get;
+        set;
+    }
+
+    public int Minutes
+    {
+        get;
+        set;
+    }
+}
